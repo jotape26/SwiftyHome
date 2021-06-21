f904a857ba3570d3c2e3acd4460ed693ebc01b73
João Pedro Leite <joaopleite26@gmail.com>
2021-06-21 10:31:29 -0300

    Initial Commit

---
+++ SwiftyHome/SwiftyHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyHomeApp.swift
+//  SwiftyHome
+//
+//  Created by João Leite on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
